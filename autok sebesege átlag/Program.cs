2016cfd3e5eba464f3fe7095af3db58866acd77a
Ádám Plesoczki <43034243+plesoczkiadam@users.noreply.github.com>
2022-12-06 08:32:09 +0100

    Add files via upload

---
+++ autok sebesege átlag/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, s;
+            Console.WriteLine("Hány auto sebbeséget kérjem be?");
+            Console.Write(" autók száma : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            s = 0;
+            int[] kocsik = new int[20];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("auto sebbesége {0} : ", i + 1);
+                kocsik[i] = Convert.ToInt32(Console.ReadLine());
+                s = s + kocsik[i];
+
+            }
+            Console.WriteLine("a kocsik átlagsebbesége {0} ", s/n);
+            Console.ReadLine();
+        }
+    }
+}
